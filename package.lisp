7fedfc9c96859fde2dbd4bfeb7b9776a1d676ca3
Baggers <chris.bagley@gmail.com>
2017-06-02 02:25:17 +0200

    use vari

--- package.lisp
+++ package.lisp
@@ -2,12 +2,12 @@
 
 (uiop:define-package #:cepl.camera.generics
     (:use :cl :cepl :cepl.spaces
-           :varjo :varjo-lang)
+           :varjo :vari)
   (:export :cam->clip :x->cam))
 
 (uiop:define-package #:cepl.camera.base
     (:use :cl :cepl :rtg-math :cepl.spaces #:cepl.camera.generics
-           :varjo :varjo-lang)
+           :varjo :vari)
   (:export :make-base-camera :update-cam->clip :base-camera
            :with-base-camera :in-space :near :far :fov
            :camera-viewport :camera-dimensions :camera-resolution
@@ -15,7 +15,7 @@
 
 (uiop:define-package #:cepl.camera
     (:use :cl :cepl :rtg-math :cepl.spaces #:cepl.camera.generics
-          #:cepl.camera.base :varjo :varjo-lang)
+          #:cepl.camera.base :varjo :vari)
   (:import-from :cepl.camera.base :perspective)
   (:export :camera :make-camera :in-space :near :far :fov
            :cam->clip :x->cam :using-camera :camera-pos :camera-rot
